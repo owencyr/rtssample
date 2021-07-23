8ab5d954a42b56a76da5ec66890a15b7b45cb640
owencyr <cyroc@dukes.jmu.edu>
2021-07-22 22:33:49 -0400

    working on pagination

--- src/Search.js
+++ src/Search.js
@@ -2,13 +2,28 @@ import React, { useState } from 'react';
 import { getByDate } from './api/hackerNews';
 
 export default function Search({ addSearchHistory }) {
-  const handleSearch = async (event) => {
-    event.preventDefault();
-    console.log({ searchTerm });
-    const articles = await getByDate(searchTerm);
-    if (articles) {
-      addSearchHistory(searchTerm);
-      updateSearchResults(articles);
+  const handleSubmit = async (event = { preventDefault: () => {} }) => {
+    try {
+      event.preventDefault();
+      const searchTermValid = searchTerm !== '';
+      console.log({ searchTerm });
+      if (searchTermValid) {
+        const {
+          searchResults = [],
+          searchResultsPage = 0,
+          totalPages = 0,
+        } = await getByDate(searchTerm);
+        if (searchResults && searchResults.length) {
+          addSearchHistory(searchTerm);
+          updateSearchResults(searchResults);
+          updateSearchResultsPage(searchResultsPage);
+          updateTotalSearchResultsPages(totalPages);
+        }
+      } else {
+        throw new Error('Not a valid search term');
+      }
+    } catch (e) {
+      updateErrorMessage(e);
     }
   };
 
@@ -22,19 +37,57 @@ export default function Search({ addSearchHistory }) {
     });
   };
 
+  const nextPage = () => {
+    updateSearchResultsPage(searchResultsPage + 1);
+    handleSubmit(searchResultsPage);
+  };
+
+  const previousPage = () => {
+    const pageToUpdateTo = searchResultsPage > 1 ? searchResultsPage - 1 : 1;
+    updateSearchResultsPage(pageToUpdateTo);
+    handleSubmit(searchResultsPage);
+  };
+
   const [searchTerm, updateSearchTerm] = useState('');
   const [searchResults, updateSearchResults] = useState([]);
-  console.log({ searchTerm });
+  const [searchResultsPage, updateSearchResultsPage] = useState(0);
+  const [totalSearchResultsPages, updateTotalSearchResultsPages] = useState(0);
+  const [errorMessage, updateErrorMessage] = useState('');
+
+  const previousPageButtonDisabled = searchResults.length === 0 || searchResultsPage === 0;
+  const nextPageButtonDisabled =
+    searchResults.length === 0 ||
+    totalSearchResultsPages === 0 ||
+    searchResultsPage === totalSearchResultsPages;
+
+  console.log({ searchResults });
   return (
     <div>
-      <form onSubmit={(event) => handleSearch(event)}>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <input
           type="text"
-          placeholder="search For articles here"
+          placeholder="Search for articles"
           value={searchTerm}
           onChange={(event) => updateSearchTerm(event.target.value)}
         ></input>
+        <div style={{ visibility: errorMessage === '' ? 'hidden' : 'visible' }}>{errorMessage}</div>
         <input type="submit"></input>
+        <button
+          id="previous-page-button"
+          type="button"
+          onClick={previousPage}
+          disabled={previousPageButtonDisabled}
+        >
+          Previous Page
+        </button>
+        <button
+          id="next-page-button"
+          type="button"
+          onClick={nextPage}
+          disabled={nextPageButtonDisabled}
+        >
+          Next Page
+        </button>
       </form>
       <ul>{searchResults ? renderSearchResults(searchResults) : null}</ul>
     </div>
